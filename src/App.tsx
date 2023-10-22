dcc7f9ced215573f32fee8aacfc6f8b4d3a399cc
wrzesinski-hubert <wrzesinski.hubert@gmail.com>
2023-10-22 13:58:52 +0200

    add Material UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import WheelOfFortune from "./components/WheelOfFortune/WheelOfFortune";
+import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+import { useThemeContext } from "./theme/ThemeContextProvider";
+import { CssBaseline } from "@mui/material";
+import NightModeToggle from "./components/NightModeToggle/NightModeToggle";
 
 function App() {
+  const { theme } = useThemeContext();
+
   return (
-    <>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NightModeToggle />
       <WheelOfFortune />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/components/WheelOfFortune/WheelOfFortune.tsx
+++ src/components/WheelOfFortune/WheelOfFortune.tsx
@@ -1,11 +1,27 @@
-import React, { useState } from "react";
+import { useState } from "react";
+import Box from "@mui/material/Box";
+import AbcIcon from "@mui/icons-material/Abc";
+import PercentIcon from "@mui/icons-material/Percent";
+import ColorLensIcon from "@mui/icons-material/ColorLens";
+import TextField from "@mui/material/TextField";
 import "./style.scss";
+import { Stack } from "@mui/material";
+import { styled } from "@mui/material/styles";
+import Paper from "@mui/material/Paper";
 
 interface Slice {
   name: string;
   percent: string;
   color: string;
 }
+
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: "center",
+  color: theme.palette.text.secondary,
+}));
 
 function WheelOfFortune() {
   const [slices, setSlices] = useState<Slice[]>([]);
@@ -55,12 +71,16 @@ function WheelOfFortune() {
       )
     );
   };
+  const hanldeNameChange = (index: number, newValue: string) => {
+    setSlices((prevSlices) =>
+      prevSlices.map((slice, i) =>
+        i === index ? { ...slice, name: newValue } : slice
+      )
+    );
+  };
 
   const addSlice = () => {
-    setSlices((prev) => [
-      ...prev,
-      { name: "test", percent: "10%", color: "red" },
-    ]);
+    setSlices((prev) => [...prev, { name: "", percent: "10%", color: "red" }]);
   };
 
   return (
@@ -82,17 +102,44 @@ function WheelOfFortune() {
       <div className="inputsWrapper">
         {slices.map((slice, index) => (
           <div className="singleInputWrapper" key={index}>
-            <input value={slice.name} readOnly />
-            :
-            <input
-              value={slice.percent}
-              onChange={(e) => handlePercentChange(index, e.target.value)}
-            />
-            :
-            <input
-              value={slice.color}
-              onChange={(e) => handleColorChange(index, e.target.value)}
-            />
+            <Stack spacing={2}>
+              <Item sx={{ display: "flex", flexDirection: "column" }}>
+                <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                  <AbcIcon sx={{ color: "action.active", mr: 1, my: 0.5 }} />
+                  <TextField
+                    id="input-with-sx"
+                    label="Nazwa"
+                    variant="standard"
+                    value={slice.name}
+                    onChange={(e) => hanldeNameChange(index, e.target.value)}
+                  />{" "}
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                  <PercentIcon
+                    sx={{ color: "action.active", mr: 1, my: 0.5 }}
+                  />
+                  <TextField
+                    id="input-with-sx"
+                    label="Nazwa"
+                    variant="standard"
+                    value={slice.percent}
+                    onChange={(e) => handlePercentChange(index, e.target.value)}
+                  />
+                </Box>
+                <Box sx={{ display: "flex", alignItems: "flex-end" }}>
+                  <ColorLensIcon
+                    sx={{ color: "action.active", mr: 1, my: 0.5 }}
+                  />
+                  <TextField
+                    id="input-with-sx"
+                    label="Nazwa"
+                    variant="standard"
+                    value={slice.color}
+                    onChange={(e) => handleColorChange(index, e.target.value)}
+                  />
+                </Box>
+              </Item>
+            </Stack>
           </div>
         ))}
 
